a5ebe2f5df63b1bb7e4151c12fe724a4c3d291ea
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-10-21 09:17:34 +0000

    task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+/**
+ * main - prints the size of various types
+ *
+ * Return: O
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
+}
