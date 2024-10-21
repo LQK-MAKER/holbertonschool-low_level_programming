5e81f122fe3a36cbaa2cb3d536a7019f0a3297ab
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-10-21 15:29:45 +0000

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main-Numberz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 15; a < 25; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (o);
+}
