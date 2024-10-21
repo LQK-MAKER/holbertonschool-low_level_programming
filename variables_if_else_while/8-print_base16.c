8187afd927010f31c2e05a9a7be7664870f7dde9
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-10-21 16:10:07 +0000

    hot

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -9,12 +9,13 @@ int main(void)
 	int n;
 	char l;
 
-	for (n = 0; n < 10; n++Â)
+	for (n = 0; n < 10; n++)
 		putchar((n % 10) + '0');
 
 	for (l = 'a'; l <= 'f'; l++)
 		putchar(l);
 
 	putchar('\n');
-
+	
 	return (0);
+}
