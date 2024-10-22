0f5b75bae3f8b8bb1165a8aa78a1962d8eabdcf6
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-10-22 11:49:29 +0000

    task 1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Main - Prints alphabet
+ *
+ *Return: 0
+ */
+void print_alphabet(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		_putchar(l);
+
+	_putchar('\n');
+}
