b17cba45a4f4d900beb23587db0d5f6127edee1a
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-11-04 10:20:37 +0000

    all task

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -9,7 +9,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned in a;
+	unsigned int a;
 
 	for (a = 0; a < n; a++)
 	{
@@ -17,4 +17,4 @@ char *_memset(char *s, char b, unsigned int n)
 	}
 
 	return (s);
-
+}
