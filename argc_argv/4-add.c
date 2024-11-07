2317a3cbcd8565cf7e8dc696e677fe56302cf1e6
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-11-07 09:04:54 +0000

    task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+		if (argc == 1)
+		{
+			printf("0\n");
+			return (0);
+		}
+	for (int i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
