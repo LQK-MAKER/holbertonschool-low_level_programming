759154f39f55c8d6632c41500f971224cc497535
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-11-19 08:57:02 +0000

    all tasks

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - Return the
+ * @a: the first nymbers
+ * @b: the second number
+ *
+ * Return: the sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Return the
+ * @a: the first nymbers
+ * @b: the second number
+ *
+ * Return: the sum of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Return the
+ * @a: the first nymbers
+ * @b: the second number
+ *
+ * Return: the sum of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Return the
+ * @a: the first nymbers
+ * @b: the second number
+ *
+ * Return: the sum of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Return the
+ * @a: the first nymbers
+ * @b: the second number
+ *
+ * Return: the sum of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
