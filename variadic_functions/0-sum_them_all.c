0ece2a4a6dfd2c964e279e93f87b9b8c61fb9e83
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-11-21 14:58:04 +0000

    task 0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_function.h"
+#include <stdarg.h>
+/**
+ *sum_them_all - return the sum
+ *@n: the numbers
+ *@...: a variable
+ *
+ * Return: if n = 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list tartenpion;
+	unsigned int tibers, sum = 0;
+
+	va_start(tartenpion, n);
+
+	for (tibers = 0; tibers < n; tibers++)
+		sum += va_arg(tibers, int);
+
+	va_end(tibers);
+
+	return (sum);
+}
