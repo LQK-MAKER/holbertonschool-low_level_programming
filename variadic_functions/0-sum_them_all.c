4d56064962d335c15d7c9974454956797aa7ee99
LQK-MAKER <lesmayoux.theo@gmail.com>
2024-11-22 19:40:42 +0000

    correction

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -13,18 +13,18 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int p;
 	int y;
 	int result = 0;
-	va_list sum;
+	va_list tartenpion;
 
-	va_start(sum, n);
+	va_start(tartenpion, n);
 
 	if (n == 0)
 		return (0);
 
 	for (p = 0; p < n; p++)
 	{
-		y = va_arg(sum, int);
+		y = va_arg(tartenpion, int);
 		result += y;
 	}
-	va_end(sum);
+	va_end(tartenpion);
 	return (result);
 }
